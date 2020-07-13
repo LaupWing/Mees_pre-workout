44ca0f68a7b1d863642cfc2e51cd8b97f4af5b8a
LaupWing <loc_strict@hotmail.com>
2020-07-13 13:40:14 +0200

    update

--- components/header/header.js
+++ components/header/header.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './header.module.css'
 
 const Header = () => (
-    <header>
+    <header className={styles.header}>
         <img src={`/logo.jpg`}/>
     </header>
 )
