e6f1f83cb807bb23313487a7d47dacbc3ece5e87
LaupWing <loc_strict@hotmail.com>
2020-07-14 17:52:11 +0200

    added footer content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from './footer.module.css'
+import {Link} from 'gatsby'
 
 const Footer = ()=>{
     return(
         <footer className={styles.footer}>
-
+            <div className={'container ' + styles.content}>
+                <ul>
+                    <li><Link activeClassName={styles.active} to="/">Home</Link></li>
+                    <li><Link activeClassName={styles.active} to="/producten">Producten</Link></li>
+                    <li><Link activeClassName={styles.active} to="/over_ons">Over Ons</Link></li>
+                    <li><Link activeClassName={styles.active} to="/koopvoorwaarden">Koopvoorwaarden</Link></li>
+                    <li><Link activeClassName={styles.active} to="/contact">Contact</Link></li>
+                </ul>
+            </div>
         </footer>
     )
 }
