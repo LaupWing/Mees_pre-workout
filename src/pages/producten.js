c8b82215e90fa41287632ba3d0148c9a56e9790d
LaupWing <loc_strict@hotmail.com>
2020-07-14 13:41:16 +0200

    producten query

--- src/pages/producten.js
+++ src/pages/producten.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
 
 const Producten = ()=>{
+    const producten = useStaticQuery(graphql`
+        query{
+            allContentfulProduct(
+                sort:{
+                    fields:datum,
+                    order: DESC
+                }
+                ){
+                totalCount
+                edges{
+                    node{
+                    productNaam
+                        foto{
+                            file{
+                                url
+                            }
+                        }
+                        prijs
+                        datum(fromNow:true)
+                    }
+                }
+            }
+        }
+    `)
+    console.log(producten)
     return(
         <div><h1>Dit is de Producten pagina</h1></div>
     )
